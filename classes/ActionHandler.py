3f8b316a5641734e03877110a7ca1baff14371ad
Colberr <30664849+Colberr@users.noreply.github.com>
2022-08-03 15:51:33 +0100

    Button presses

--- classes/ActionHandler.py
+++ classes/ActionHandler.py
@@ -3,8 +3,12 @@ import json
 from datetime import datetime
 
 # Keybinds
-SWAP_FEEDS = [pygame.K_o, pygame.CONTROLLER_BUTTON_A]
-CYCLE_CAM_MODE = [pygame.K_p, pygame.CONTROLLER_BUTTON_B]
+SWAP_FEEDS = [pygame.K_o]
+CYCLE_CAM_MODE = [pygame.K_p]
+BUTTON_A = [pygame.CONTROLLER_BUTTON_A] # Can rename these
+BUTTON_B = [pygame.CONTROLLER_BUTTON_B]
+BUTTON_X = [pygame.CONTROLLER_BUTTON_X]
+BUTTON_Y = [pygame.CONTROLLER_BUTTON_Y]
 
 # ActionHandler class
 class ActionHandler:
@@ -20,12 +24,22 @@ class ActionHandler:
 		'''Changes the currently stored input controller'''
 		self.Controller = new_cont
 
+	def send_msg(self, msg):
+		'''Convert a message into json string and try to send it to the rover'''
+		# Prepend current time
+		msg.insert(0, datetime.now().strftime("%H:%M:%S.%f")[:-3])
+
+		# Catch exception if msg cannot be sent
+		try:
+			# Convert list to JSON string then encode to bytes and send
+			self.soc.sendall(json.dumps(msg).encode())
+		except:
+			# TODO: Log this somehow
+			return
+
 	def send_commands(self):
 		'''Send commands to rover if the controller is being acted on'''
-		# Set up msg with current time
-		msg = [
-			datetime.now().strftime("%H:%M:%S.%f")[:-3]
-		]
+		msg = []
 
 		# LJOY (VERTICAL) - MOVE FORWARD/ BACKWARDS
 		if abs(self.Controller.axes[1]) > 0.01:
@@ -59,14 +73,8 @@ class ActionHandler:
 			msg.append({"BRUSH": trigger_dir * self.Controller.axes[5]})
 
 		# Only send if commands were added
-		if len(msg) > 1:
-			# Catch exception if msg cannot be sent
-			try:
-				# Convert list to JSON string then encode to bytes and send
-				self.soc.sendall(json.dumps(msg).encode())
-			except:
-				# TODO: Log this somehow
-				return
+		if len(msg) > 0:
+			self.send_msg(msg)
 
 	def button_press(self, b):
 		'''Calls a function after button on keyboard or controller is pressed'''
@@ -74,6 +82,14 @@ class ActionHandler:
 			self.swap_feeds()
 		elif b in CYCLE_CAM_MODE:
 			self.cycle_cam_mode()
+		elif b in BUTTON_A:
+			self.button_function("A")
+		elif b in BUTTON_B:
+			self.button_function("B")
+		elif b in BUTTON_X:
+			self.button_function("X")
+		elif b in BUTTON_Y:
+			self.button_function("Y")
 
 	def swap_feeds(self):
 		'''Swaps camera feeds'''
@@ -82,3 +98,8 @@ class ActionHandler:
 	def cycle_cam_mode(self):
 		'''Cycles between ways organising camera feeds'''
 		self.FeedManager.cycle_mode()
+
+	def button_function(self, button):
+		'''[Template] A function triggered by a controller button'''
+		command = {"BUTTON_PRESSED": button}
+		self.send_msg([command])
