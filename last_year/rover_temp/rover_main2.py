f1c068ca6110460bbafeb187343f3cc3447f16c6
Colberr <30664849+Colberr@users.noreply.github.com>
2023-01-06 10:20:22 +0000

    Edits from testing

--- last_year/rover_temp/rover_main2.py
+++ last_year/rover_temp/rover_main2.py
@@ -26,71 +26,79 @@ def listen_function(control_queue):
 
 def main_function(control_queue):
 
-	# Set up cameras (at the moment code only works with exactly 2, this will be changed)
-	captures = [cv2.VideoCapture(0), cv2.VideoCapture(0)]
-	PAYLOAD_STRING = "<HII"
+	try:
 
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sendsock:
+		# Set up cameras (at the moment code only works with exactly 2, this will be changed)
+		captures = [cv2.VideoCapture(0), cv2.VideoCapture(1)]
+		PAYLOAD_STRING = "<HII"
 
-		# Connect to laptop
-		s = SendSocket(sendsock, LAPTOP_IP, 5002, PAYLOAD_STRING)
-		s.connect()
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sendsock:
 
-		# Send confirmation message
-		msg = {"Connected": True}
-		s.send(msg, [np.array([])] * 2)
+			# Connect to laptop
+			s = SendSocket(sendsock, LAPTOP_IP, 5002, PAYLOAD_STRING)
+			s.connect()
 
-		# ======
+			# Send confirmation message
+			msg = {"Connected": True}
+			s.send(msg, [np.array([])] * 2)
 
-		prev_time = time.perf_counter()
-		freq = 1/20
+			# ======
 
-		control_list = []
+			prev_time = time.perf_counter()
+			freq = 1/20
 
-		# Main rover loop
-		while True:
+			control_list = []
 
-			# Unload control instructions
-			while not control_queue.empty():
-				instruction = control_queue.get()
-				if instruction == "QUIT_ROVER":
-					print("Exiting")
-					for cap in captures: cap.release()
-					raise SystemExit
-				else:
-					control_list.append(instruction)
+			# Main rover loop
+			while True:
 
-			# [Temp] Showing control instructions
-			if control_list:
-				print(control_list)
-				control_list = []
-
-			# Send feedback and images
-			if time.perf_counter() - prev_time > freq:
-				
-				# [Temp] Feedback
-				i = randint(0, 1000)
-				if i < 100:
-					fb = {"Test feedback": f"{i}"}
-				else:
-					fb = {}
-
-				# Camera
-				frames = []
-				for cap in captures:
-					ret, frame = cap.read()
-
-					if not ret:
-						print("Skipped frame")
-						frame = np.array([])
+				# Unload control instructions
+				while not control_queue.empty():
+					instruction = control_queue.get()
+					if instruction == "QUIT_ROVER":
+						print("Exiting")
+						for cap in captures: cap.release()
+						raise SystemExit
 					else:
-						sf = 0.4 # Scale factor
-						frame = cv2.resize(frame, (0, 0), fx=sf, fy=sf)
+						control_list.append(instruction)
 
-					frames.append(frame)
+				# [Temp] Showing control instructions
+				if control_list:
+					print(control_list)
+					control_list = []
 
-				s.send(fb, frames)
-				prev_time = time.perf_counter()
+				# Send feedback and images
+				if time.perf_counter() - prev_time > freq:
+					
+					# [Temp] Feedback
+					i = randint(0, 1000)
+					if i < 100:
+						fb = {"Test feedback": f"{i}"}
+					else:
+						fb = {}
+
+					# Camera
+					frames = []
+					for i, cap in enumerate(captures):
+						ret, frame = cap.read()
+
+						if not ret:
+							print(f"Skipped frame {i}")
+							frame = np.array([])
+						else:
+							frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+							sf = 0.4 # Scale factor
+							frame = cv2.resize(frame, (0, 0), fx=sf, fy=sf)
+
+						frames.append(frame)
+
+					s.send(fb, frames)
+					prev_time = time.perf_counter()
+
+	except KeyboardInterrupt:
+		print(" KeyboardInterrupt caught")
+		for cap in captures: cap.release()
+		raise SystemExit
 
 if __name__ == "__main__":
 	control_queue = queue.Queue(0)
