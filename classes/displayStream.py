bf9f436b09086f79b5dccbe5205ee83d0ab1d0e9
PixelMutation <charlie.e.spencer@gmail.com>
2023-03-12 16:34:05 +0000

    add fast UDP stream for mjpeg cams

--- classes/displayStream.py
+++ classes/displayStream.py
@@ -8,9 +8,12 @@ import sys
 class camera:
     process=None
     host="stereocam"
-    def __init__(self,name,port,width,height,stereo=False):
+    frame=None
+    def __init__(self,name,host,type,port,width,height,stereo=False):
         self.name=name
+        self.host=host
         self.port=port
+        self.type=type
         self.width=width
         self.height=height
         self.stereo=stereo
@@ -22,14 +25,19 @@ class camera:
         else:
             return True
     def start(self):
+        options=""
+        if self.type=="udp":
+            options="?buffer_size=10000"
+        else:
+            options="?tcp_nodelay=1"
         command = ['ffmpeg.exe',
             '-hide_banner',
-            '-probesize','32',
+            '-probesize','500000',
             '-analyzeduration','0',
             '-flags', 'low_delay',
             '-strict','experimental',
             '-hwaccel','auto',
-            '-i', f'tcp://{self.host}:{self.port}',
+            '-i', f'{self.type}://{self.host}:{self.port}{options}',
             '-vf',f"scale={self.width}:{self.height}",
             '-fflags', "nobuffer",
             '-f', 'rawvideo',      # Get rawvideo output format.
@@ -39,55 +47,70 @@ class camera:
             self.process.kill()
         except:
             pass
-        self.process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        self.process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
     def read(self):
         raw_frame = self.process.stdout.read(self.width*self.height*3)
-        return np.frombuffer(raw_frame, np.uint8).reshape((self.height, self.width, 3))
+        self.frame= np.frombuffer(raw_frame, np.uint8).reshape((self.height, self.width, 3))
+        return self.frame
     def display(self):
         cv2.imshow(self.name, self.read())
     def stop(self):
         self.process.stdout.close()  # Closing stdout terminates FFmpeg sub-process.
         self.process.wait()  # Wait for FFmpeg sub-process to finish
 
-cameras=[
-    camera("Stereo",8081,1296,972,stereo=True),
-    camera("USB",8082,640,480)
-    ]
 
-camthreads=[]
-
-def runcamera(cam):
-    cam.start()
-    while True:
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        if not cam.running():
-            cam.start()
-        try:
-            cam.display()
-        except:
-            pass
-    cam.stop()
-
-
-for cam in cameras:
-    camthreads.append(threading.Thread(target=runcamera,args=(cam,),daemon=True))
-
-for thread in camthreads:
-    thread.start()
 
 def stop():
     cv2.destroyAllWindows()
     sys.exit()
 
-while True:
-    try:
-        running=0
-        for thread in camthreads:
-            if thread.is_alive():
-                running+=1
-        if running==0:
+def runcamera(cam):
+    cam.start()
+    while True:
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+        if not cam.running():
+            cam.stop()
+            cam.start()
+        try:
+            cam.read()
+            # cam.display()
+        except:
+            pass
+    cam.stop()
+
+
+cameras=[
+        camera("Stereo","stereocam","tcp",8081,640,480,stereo=True),
+        # camera("Stereo","127.0.0.1","udp",8081,640,480,stereo=True),
+        # camera("USB","stereocam","tcp",8082,640,480),
+        camera("USB","127.0.0.1","udp",8082,640,480)
+        ]
+
+camthreads=[]
+
+def runCams():
+    while True:
+        try:
+            running=0
+            for thread in camthreads:
+                if thread.is_alive():
+                    running+=1
+            if running<2:
+                stop()
+            time.sleep(.3)
+        except KeyboardInterrupt:
             stop()
-        time.sleep(.3)
-    except KeyboardInterrupt:
-        stop()
+            break
+
+daemon_thread=None
+
+def startCams():
+    for cam in cameras:
+        camthreads.append(threading.Thread(target=runcamera,args=(cam,),daemon=True))
+
+    for thread in camthreads:
+        thread.start()
+
+    daemon_thread=threading.Thread(target=runCams,daemon=True)
+
